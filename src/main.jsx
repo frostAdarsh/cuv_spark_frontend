43b098a4393fb3d5bd12f8cac9f22d338ca119c4
frostAdarsh <adarshmani017@gmail.com>
2025-03-01 20:52:38 +0530

    third

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
-import { NameProvider } from "./context/NameContext.jsx";
+// import { NameProvider } from "./context/NameContext.jsx";
 import { DataContextProvider } from "./context/DataContext.jsx";
 export const server = "https://cuv-sparck-backend.onrender.com";
 createRoot(document.getElementById("root")).render(
